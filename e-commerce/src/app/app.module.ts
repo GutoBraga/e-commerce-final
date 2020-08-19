114e18fc8c9ccf6003d0878405de8b61c0207c6c
Gutemberg Braga <gutbra.arag@hotmail.com>
2020-08-19 00:16:40 -0300

    edicao pagina home

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderMainComponent } from './header-main/header-main.component';
+import { CaroselComponent } from './carosel/carosel.component';
+import { FooterMainComponent } from './footer-main/footer-main.component';
+import { MenuCategoriaComponent } from './menu-categoria/menu-categoria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderMainComponent
+    HeaderMainComponent,
+    CaroselComponent,
+    FooterMainComponent,
+    MenuCategoriaComponent
   ],
   imports: [
     BrowserModule
