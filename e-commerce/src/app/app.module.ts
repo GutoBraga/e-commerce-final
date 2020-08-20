4b9715fdb9cf4766de084a8748cf2a4fcf3024f2
GutoBraga <gutbra.arag@hotmail.com>
2020-08-19 23:08:19 -0300

    Update app.module.ts

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HistComprasComponent } from './hist-compras/hist-compras.component';
-
+import { HeaderMainComponent } from './header-main/header-main.component';
  
 
 @NgModule({
   declarations: [
     AppComponent,
-    HistComprasComponent
- 
+    HistComprasComponent,
+    HeaderMainComponent
   ],
   imports: [
     BrowserModule
