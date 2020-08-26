08d55d950571be592e9ce17d5ef4341d27e2102b
Ricardo de Oliveira <ricardoliveira50@hotmail.com>
2020-08-26 18:20:02 -0300

    Rotas todas as telas V1

--- e-commerce/src/app/header-main/header-main.component.ts
+++ e-commerce/src/app/header-main/header-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+ 
 
 @Component({
   selector: 'app-header-main',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderMainComponent implements OnInit {
 
   constructor() { }
+
+  
 
   ngOnInit(): void {
   }
